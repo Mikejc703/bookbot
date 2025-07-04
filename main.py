3c0ad068575ed7d262fdd03797aace31af7d1941
Mikejc703 <michael.j.cunningham10@gmail.com>
2025-07-04 07:15:45 -0400

    heyyyy now

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from stats import (
     chars_dict_to_sorted_list,
     get_chars_dict,
 )
+import sys
+
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -32,5 +34,8 @@ def print_report(book_path, num_words, chars_sorted_list):
 
     print("============= END ===============")
 
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)  # Exit the program early
 
 main()
